b1c6fc023f45545ac263881d77e5c406f82ea71b
atmax1234 <34765187+atmax1234@users.noreply.github.com>
2021-06-06 18:35:27 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,22 +10,31 @@ module.exports = {
         const embed = new MessageEmbed()
         .setColor('RANDOM')
         .setTitle('Thoose are my commands:')
-        .setDescription(`**Main Commands:**\n\n
-        **!help** - Prints all the information about my commands\n
-        **!addrole <member> <role>** - You can add a role to a specific member by using this command!\n
-        **!removerole <member> <role>** - You can remove a role from a specific member by using this command!\n
-        **!members** - Check the amount of members in our server!\n
-        **!suggest <suggestion>** - Use this command to give us an idea about our server!
-        **Fun Commands (More COMING SOON!):**\n\n
-        **!8ball <question>** - Ask me anything!\n
-        **Marioo** - If your message contains Marioo or marioo it will print something funny :D\n
-        **!Hello** - Greeting command :)\n\n
-        **Info Commands**\n\n
-        **!botinfo** - Learn more about me!\n
-        **!embed** - Here I was created!\n
-        **!ping** - Check out my ping!\n
-        **!serverinfo** - Learn more about this server!\n
-        **!uptime** - Check out my uptime!\n
-        **!userinfo**/**!userinfo <user>** - Prints personal information or information about another member!`);
+		.addField('**Main Commands:**', ' ', true)
+		.addFields(
+		{ name: '**!help**', value: 'Prints all the information about my commands' },
+		{ name: '**!addrole <member> <role>**', value: 'You can add a role to a specific member by using this command!' },
+		{ name: '**!removerole <member> <role>**', value: 'You can remove a role from a specific member by using this command!' },
+		{ name: '**!members**', value: 'See the amount of members in our server!' },
+		{ name: '**!suggest <suggestion>**', value: 'Use this command to give us an idea about our server!' },
+		)
+		.addField('**Fun Commands (More COMING SOON!):**', ' ', true)
+		.addFields(
+		{ name: '**!8ball <question>**', value: 'Ask me anything!' },
+		{ name: '**Marioo**', value: 'If your message contains Marioo it will print something funny :D' },
+		{ name: '**!Hello**', value: 'Greeting command :)' },
+		{ name: '**!members**', value: 'See the amount of members in our server!' },
+		{ name: '**!suggest <suggestion>**', value: 'Use this command to give us an idea about our server!' },
+		)
+		.addField('**Info Commands**', ' ', true)
+		.addFields(
+		{ name: '**!botinfo**', value: 'Learn more about me!' },
+		{ name: '**!embed**', value: 'Here I was created!' },
+		{ name: '**!ping**', value: 'Check out my ping!' },
+		{ name: '**!serverinfo***', value: 'Learn more about this server!' },
+		{ name: '**!uptime**', value: 'Check out my uptime!' },
+		{ name: '**!userinfo**/**!userinfo <user>**', value: 'Prints personal information or information about another member that you mentioned!' },
+		)
+        message.channel.send(embed)
     }
 };
