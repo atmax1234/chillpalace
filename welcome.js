44323c3fab8861c590488dacdcbac24dc69ff216
atmax1234 <34765187+atmax1234@users.noreply.github.com>
2021-06-06 16:38:19 +0200

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,7 +1,17 @@
 module.exports = client => {
     client.on('guildMemberAdd', (member) =>{
-        let channel = message.channel.guild.channels.cache.find((channel) => channel.name.toLowerCase() === `welcome`)
-        const message = `Welcome <@${member.id}> to our server! Make sure to check out the rules channel!`
-        channel.send(message)
+        const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome');
+        if(!channel) return;
+
+        const welcomeEmbed = new Discord.MessageEmbed()
+        .setColor(pink)
+        .setAuthor('ChillPalace')
+        .setTitle('Welcome!')
+        .setDescription(`<@${member} just joined the server! Make sure to read #rules!`)
+        .setThumbnail(message.user.avatarURL)
+        .setFooter('Have fun!')
+        .setTimestamp();
+
+    channel.send(welcomeEmbed);
     })
 }
