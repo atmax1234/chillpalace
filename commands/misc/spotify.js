6faa986a3ec4a0fdcd897111edccbcd6efe003b6
atmax1234 <34765187+atmax1234@users.noreply.github.com>
2021-06-20 16:16:50 +0200

    Create spotify.js

---
+++ commands/misc/spotify.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    name: 'spotify',
+    description: 'Spotify Search',
+    aliases: ['spotify-search'],
+    args: true,
+    usage: "<song name>",
+
+    execute(message, args, client, Discord){
+        let msglink = args.join('%20')
+        let msg = args.join(' ')
+        if(!args[0]) return message.channel.send('Please give me a song name to search')
+
+        let embed = new MessageEmbed()
+            .setColor('GREEN')
+            .setDescription(`[${msg}](https://open.spotify.com/search/${msglink})`)
+            message.channel.send(embed)
+        }
+}
